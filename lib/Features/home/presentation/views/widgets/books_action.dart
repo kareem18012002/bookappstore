a33be605c14838d849d3e3326af18ac4fc67651f
kareemswilm <kareemswilm9@gmail.com>
2024-07-11 14:11:17 +0300

    Initial commit

---
+++ lib/Features/home/presentation/views/widgets/books_action.dart
@@ -0,0 +1,65 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+import 'package:bookly/core/utils/function/launch_url.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import '../../../../../core/widgets/custom_button.dart';
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key, required this.bookModel});
+
+  final BookModel bookModel;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: [
+          const Expanded(
+            child: CustomButton(
+              text: 'Free',
+              backgroundColor: Colors.white,
+              textColor: Colors.black,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(16),
+                bottomLeft: Radius.circular(16),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CustomButton(
+              onPressed: () {
+                if (bookModel.volumeInfo.previewLink != null) {
+                  launchCustomUr(context, bookModel.volumeInfo.previewLink!);
+                } else {
+                  // يمكنك عرض رسالة أو تنفيذ أي إجراء مناسب في حال عدم وجود الرابط
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Preview link is not available'),
+                    ),
+                  );
+                }
+              },
+              fontSize: 16,
+              text: getText(bookModel),
+              backgroundColor: const Color(0xffEF8262),
+              textColor: Colors.white,
+              borderRadius: const BorderRadius.only(
+                topRight: Radius.circular(16),
+                bottomRight: Radius.circular(16),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null) {
+      return 'Not Available';
+    } else {
+      return 'Preview';
+    }
+  }
+}
